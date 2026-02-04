ff1f527ea491adcf3f06c754de8b41f603ab11b5
sara ashmoz <sarashmoz@gmail.com>
2026-02-04 10:13:04 +0200

    button

--- test/pages/dashboard_page.py
+++ test/pages/dashboard_page.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Page, expect
+import re
 
 class DashboardPage:
     def __init__(self, page: Page):
@@ -15,6 +16,23 @@ class DashboardPage:
         expect(self.h_history).to_be_visible()
 
     def open_send_alert_first_customer(self):
-        expect(self.btn_send_alert_first).to_be_visible(timeout=15000)
-        self.btn_send_alert_first.click()
-        expect(self.h_send_alert).to_be_visible()
+        expect(
+        self.page.locator("table tbody tr").first ).to_be_visible(timeout=30000)
+
+    # רק עכשיו מחפשים כפתור
+        btn = self.page.get_by_role(
+            "button",
+            name=re.compile("send alert", re.I)
+        ).first
+
+        expect(btn).to_be_visible(timeout=30000)
+        btn.click()
+
+
+
+
+
+        
+        # expect(self.btn_send_alert_first).to_be_visible(timeout=15000)
+        # self.btn_send_alert_first.click()
+        # expect(self.h_send_alert).to_be_visible()
--- test/test_send_alert_e2e.py
+++ test/test_send_alert_e2e.py
@@ -4,6 +4,14 @@ from test.pages.send_alert_modal import SendAlertModal
 def test_send_alert_e2e_success(dashboard_page):
     page = dashboard_page
 
+    try:
+        visit_btn = page.get_by_role("button", name="Visit Site")
+        if visit_btn.is_visible():
+            visit_btn.click()
+            page.wait_for_load_state("domcontentloaded")
+    except:
+        pass
+
     dashboard = DashboardPage(page)
     send_modal = SendAlertModal(page)
 
